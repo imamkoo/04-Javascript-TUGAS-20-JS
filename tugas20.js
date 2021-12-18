9588486289be5284af058b40d5e67909da5d6a8e
Imamkoo <imammm.kooo@gmail.com>
2021-12-18 17:06:59 +0700

    Tugas 20 Javascript

---
+++ tugas20.js
@@ -0,0 +1,4 @@
+let search = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+
+console.log(search.match('are'));
+console.log(search.match('words'));
